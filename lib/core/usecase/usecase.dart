31f2147f151b78179c4456de00fd7e901d811585
msyamsularif <syamsularif0904@gmail.com>
2022-03-15 13:48:42 +0700

    Update core folder

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -1,5 +1,13 @@
-import 'package:airplane/core/values/values.dart';
+import 'package:dartz/dartz.dart';
+import 'package:equatable/equatable.dart';
+
+import '../error/failures.dart';
 
 abstract class UseCase<Type, Params> {
-  Future<ApiReturnValue> call(Params params);
+  Future<Either<Failure, Type>> call(Params params);
+}
+
+class NoParams extends Equatable {
+  @override
+  List<Object> get props => [];
 }
