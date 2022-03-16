686271c0fcbe187325f548e8f7f27b082e7ea6a8
msyamsularif <syamsularif0904@gmail.com>
2022-03-16 13:50:55 +0700

    Implement flutter lifecycle for hide content if Applifecycle.inactive in wallet page

--- lib/presentation/pages/wallet_page.dart
+++ lib/presentation/pages/wallet_page.dart
@@ -5,100 +5,129 @@ import 'package:intl/intl.dart';
 import '../../core/shared/theme.dart';
 import '../cubits/auth/auth_cubit.dart';
 
-class WalletPage extends StatelessWidget {
+class WalletPage extends StatefulWidget {
   const WalletPage({Key? key}) : super(key: key);
 
   @override
+  State<WalletPage> createState() => _WalletPageState();
+}
+
+class _WalletPageState extends State<WalletPage> with WidgetsBindingObserver {
+  AppLifecycleState stateApplifecycle = AppLifecycleState.resumed;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance!.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    setState(() {
+      stateApplifecycle = state;
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance!.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 300,
-        height: 211,
-        padding: EdgeInsets.all(defaultMargin),
-        decoration: BoxDecoration(
-          image: const DecorationImage(
-            image: AssetImage('assets/image_card.png'),
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: kPrimaryColor.withOpacity(0.5),
-              blurRadius: 50,
-              offset: const Offset(0, 10),
+    return Opacity(
+      opacity: stateApplifecycle == AppLifecycleState.inactive ? 0.0 : 1.0,
+      child: Center(
+        child: Container(
+          width: 300,
+          height: 211,
+          padding: EdgeInsets.all(defaultMargin),
+          decoration: BoxDecoration(
+            image: const DecorationImage(
+              image: AssetImage('assets/image_card.png'),
             ),
-          ],
-        ),
-        child: BlocBuilder<AuthCubit, AuthState>(
-          builder: (context, state) {
-            if (state is AuthSuccess) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Name',
-                              style: whiteTextStyle.copyWith(
-                                fontWeight: light,
+            boxShadow: [
+              BoxShadow(
+                color: kPrimaryColor.withOpacity(0.5),
+                blurRadius: 50,
+                offset: const Offset(0, 10),
+              ),
+            ],
+          ),
+          child: BlocBuilder<AuthCubit, AuthState>(
+            builder: (context, state) {
+              if (state is AuthSuccess) {
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Name',
+                                style: whiteTextStyle.copyWith(
+                                  fontWeight: light,
+                                ),
                               ),
-                            ),
-                            Text(
-                              state.user.name,
-                              style: whiteTextStyle.copyWith(
-                                fontSize: 20,
-                                fontWeight: medium,
-                              ),
-                              overflow: TextOverflow.ellipsis,
-                            )
-                          ],
-                        ),
-                      ),
-                      Container(
-                        width: 24,
-                        height: 24,
-                        margin: const EdgeInsets.only(right: 6),
-                        decoration: const BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage('assets/icon_plane.png'),
+                              Text(
+                                state.user.name,
+                                style: whiteTextStyle.copyWith(
+                                  fontSize: 20,
+                                  fontWeight: medium,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                              )
+                            ],
                           ),
                         ),
-                      ),
-                      Text(
-                        'Pay',
-                        style: whiteTextStyle.copyWith(
-                          fontSize: 16,
-                          fontWeight: medium,
+                        Container(
+                          width: 24,
+                          height: 24,
+                          margin: const EdgeInsets.only(right: 6),
+                          decoration: const BoxDecoration(
+                            image: DecorationImage(
+                              image: AssetImage('assets/icon_plane.png'),
+                            ),
+                          ),
                         ),
-                      )
-                    ],
-                  ),
-                  const Spacer(),
-                  Text(
-                    'Balance',
-                    style: whiteTextStyle.copyWith(fontWeight: light),
-                  ),
-                  Text(
-                    NumberFormat.currency(
-                      locale: 'id-ID',
-                      symbol: 'IDR ',
-                      decimalDigits: 0,
-                    ).format(
-                      state.user.balance,
+                        Text(
+                          'Pay',
+                          style: whiteTextStyle.copyWith(
+                            fontSize: 16,
+                            fontWeight: medium,
+                          ),
+                        )
+                      ],
                     ),
-                    style: whiteTextStyle.copyWith(
-                      fontSize: 26,
-                      fontWeight: medium,
+                    const Spacer(),
+                    Text(
+                      'Balance',
+                      style: whiteTextStyle.copyWith(fontWeight: light),
                     ),
-                  )
-                ],
-              );
-            } else {
-              return const SizedBox();
-            }
-          },
+                    Text(
+                      NumberFormat.currency(
+                        locale: 'id-ID',
+                        symbol: 'IDR ',
+                        decimalDigits: 0,
+                      ).format(
+                        state.user.balance,
+                      ),
+                      style: whiteTextStyle.copyWith(
+                        fontSize: 26,
+                        fontWeight: medium,
+                      ),
+                    )
+                  ],
+                );
+              } else {
+                return const SizedBox();
+              }
+            },
+          ),
         ),
       ),
     );
